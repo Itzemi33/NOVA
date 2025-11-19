993b37f725d49448a3f0c9937ec9dc98bbeb4277
Itzemi33 <fnietoc2400@alumno.ipn.mx>
2025-11-18 21:44:33 -0600

    Prueba 2

--- src/main/java/nova.java
+++ src/main/java/nova.java
@@ -2,5 +2,6 @@ public class nova {
     public static void main(String[] args) {
         System.out.println("Prueba github");
         System.out.println("Prueba github2");
+        System.out.println("Prueba github2");
     }
 }
