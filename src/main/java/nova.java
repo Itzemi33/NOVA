d2ff39b3156cc21005f164e96046b9dc5f63bf00
Itzemi33 <fnietoc2400@alumno.ipn.mx>
2025-11-18 21:57:25 -0600

    Commit 2

--- src/main/java/nova.java
+++ src/main/java/nova.java
@@ -1,6 +1,6 @@
 public class nova {
     public static void main(String[] args) {
         System.out.println("Empezemos a chambiar");
-        System.out.println("Empezemos a chambiar");
+        System.out.println("Bienvenido a NOVAAA");
     }
 }
